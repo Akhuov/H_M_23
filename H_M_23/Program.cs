df3d461fd94d9c1306eaea159e9c80ae5589a4df
user <user@WIN-F7NIMF7A3VO>
2023-06-25 08:51:05 +0500

    Добавьте файлы проекта.

---
+++ H_M_23/Program.cs
@@ -0,0 +1,21 @@
+﻿using H_M_23;
+
+
+//string folder = "D:\\Study\\.Net(Najot)\\C#\\H_M_23\\TxtFiles\\test.txt";
+//var r = new StreamWriter(folder, false, Encoding.UTF8);
+//r.WriteLine("Hello11");
+//r.Close();
+
+
+//FileStream f = new FileStream("test.txt",FileMode.Create);
+//Console.WriteLine("Nechta file yaratasiz?");
+//int son = int.Parse(Console.ReadLine());
+//Console.WriteLine("Matn kiriting");
+//string matn = Console.ReadLine();
+//StaticClass.CreateNewTxtFiles(son);
+//StaticClass.AddTextToTxtFiles(matn);
+
+
+
+StaticClass.CreateNewTxtFiles(10);
+StaticClass.AddTextToTxtFiles("Hello World!!!");
---
+++ H_M_23/StaticClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_M_23
+{
+    internal static class StaticClass
+    {
+
+        static public int? CtOf;
+        public static void CreateNewTxtFiles(int n)
+        {
+            CtOf = n;
+            for (int i = 1; i <= n; i++)
+            {
+                var path = $"D:\\Study\\.Net(Najot)\\C#\\H_M_23\\TxtFiles\\{i}.txt";
+                FileStream f = new FileStream(path, FileMode.Create);
+                f.Close();
+            }
+        }
+
+
+
+        public static void AddTextToTxtFiles(string s)
+        {
+            for (int i = 1; i <= CtOf; i++)
+            {
+                var r = new StreamWriter($"D:\\Study\\.Net(Najot)\\C#\\H_M_23\\TxtFiles\\{i}.txt", false, Encoding.UTF8);
+                r.WriteLine(s);
+                r.Close();
+            }
+        }
+
+
+
+
+    }
+}
